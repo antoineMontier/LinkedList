40b8ac36a42c5506f867479791f814238368dabc
antoine <antione.montier@sfr.fr>
2022-10-14 15:26:53 +0200

    second version (not finished)

--- linkedList.c
+++ linkedList.c
@@ -9,12 +9,67 @@ void insertHead(List *l, int x){
   Cell * c = malloc(sizeof(Cell));
   c->val = x;
   if(l->size == 0){ //manage the exception of an empty List
-    l.queue = c;
-    l.head = c;
-    l.size = 1;
+    l->queue = c;
+    l->head = c;
+    l->size = 1;
   }else{
     c->next = l-> head;
     l->head = c;
     size++;
+  }
+}
+
+int listSize(List *l){
+  if(*l == NULL){
+    return 0;
+  }
+  return l-> size;
+}
+
+void displayCell(Cell*c){
+  printf("%d", c->val);
+}
+
+void removeList(List*l){
+  Cell *p;
+  while(p != NULL){//set the pointer as the next cell of the head then delete the head then set the head as the pointed cell then re-set p as the next cell of the head and so on
+    *p = l->head->next;
+    free(l->head);
+    l->head = p;
+  }
+  free(l->head);//free the last cell
+  free(p);//free the pointer used by the while loop
+  free(l);//free the data of the list
+}
+
+int nextExists(Cell*c){
+  if(c->next != NULL){
+    return 1;
+  }else{
+    return 0;
+  }
+}
+
+int getCell(Cell*c){
+  return c->val;
+}
+
+void setCell(Cell*c, int x){
+  c->val = x;
+}
+
+void insertQueue(List*l, int x){
+  Cell*c = malloc(sizeof(Cell));
+  c->val = val;
+  c->next = NULL
+  //first, if the list is empty :
+  if(l->size == 0){
+    l->queue = c;
+    l->head = c;
+    l->size = 1;
+  }else{
+    l->queue->next = c;
+    l->queue = c;
+    l->size++;
   }
 }
--- linkedList.h
+++ linkedList.h
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-typedef struct Cell {
+typedef struct Cell {//missunderstanding of the difference between "." and "->"
   int val;
   struct Cell * next;
 }Cell;
@@ -16,22 +16,24 @@ void createCell(Cell *c);
 
 void setNext(Cell *prev, Cell *next);
 
-void setCell(Cell *c, int val);
+void setCell(Cell *c, int val);//OK, to check
 
-int getCell(Cell *c);
+int getCell(Cell *c);//implemented, to check
 
-int nextExists(Cell *c);
+int nextExists(Cell *c);//implemented to check
 
-void displayCell(Cell*c);
+void displayCell(Cell*c);//implemented, to try
 
 void createList(List * l);//implemented, to verify
 
-void removeList(List *l);
+void removeList(List *l);//implemented, to verify
 
 void insertHead(List *l, int x);//implemented, to verify
 
+void insertQueue(List*l, int x);
+
 int isEmpty(List *l);
 
-int listSize(List *l);
+int listSize(List *l);//implemented, to try
 
 void displayLis(List*l);
