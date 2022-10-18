70329acaea2d70eaaf5b2497eb73fa58f4bf8153
Antoine Montier <115585369+antoineMontier@users.noreply.github.com>
2022-10-18 20:44:55 +0200

    Version to test with a makefile

--- linkedList.c
+++ linkedList.c
@@ -1,8 +1,13 @@
-#include "LikedList.h"
+#include"linkedList.h"
+
 void createList(List *l){
   l->head = NULL;
-  l.queue = NULL;
-  size = 0;
+  l->queue = NULL;
+  l->size = 0;
+}
+
+void bla(){
+  printf("ererererere");
 }
 
 void insertHead(List *l, int x){
@@ -15,12 +20,12 @@ void insertHead(List *l, int x){
   }else{
     c->next = l-> head;
     l->head = c;
-    size++;
+    l->size++;
   }
 }
 
 int listSize(List *l){
-  if(*l == NULL){
+  if(l == NULL){
     return 0;
   }
   return l-> size;
@@ -33,7 +38,7 @@ void displayCell(Cell*c){
 void removeList(List*l){
   Cell *p;
   while(p != NULL){//set the pointer as the next cell of the head then delete the head then set the head as the pointed cell then re-set p as the next cell of the head and so on
-    *p = l->head->next;
+    p = l->head->next;
     free(l->head);
     l->head = p;
   }
@@ -60,16 +65,38 @@ void setCell(Cell*c, int x){
 
 void insertQueue(List*l, int x){
   Cell*c = malloc(sizeof(Cell));
-  c->val = val;
-  c->next = NULL
+  c->val = x;
+  c->next = NULL;
   //first, if the list is empty :
   if(l->size == 0){
     l->queue = c;
     l->head = c;
     l->size = 1;
-  }else{
+  }else{//if the list isn't empty :
     l->queue->next = c;
     l->queue = c;
     l->size++;
   }
+}
+
+void linkCell(Cell *prev, Cell *next){
+  prev->next = next;
+}
+
+int isEmpty(List *l){
+  if(l->head == NULL && l->queue == NULL){
+    return 1;
+  }else{
+    return 0;
+  }
+}
+
+void displayList(List *l){
+  Cell * displayer = l->head;
+  printf("[");
+  while(displayer->next != NULL){
+    printf("%d, ", displayer->val);//prints all but the last value
+    displayer = displayer->next;
+  }
+  printf("%d]\n", displayer->val);//prints the last value with the ending bracket
 }
